27a47f4b278cb7682735e0d01ea86fd91d504e83
tyranron <tyranron@gmail.com>
2018-03-12 23:43:20 +0200

    Test hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+#[macro_use]
+extern crate stdweb;
+
 fn main() {
-    println!("Hello, muzlo!");
+    stdweb::initialize();
+
+    let message = "Hello, 世界!";
+    js! {
+        alert(@{message});
+    }
+
+    stdweb::event_loop();
 }
