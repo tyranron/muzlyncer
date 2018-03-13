70fad53073c528922682aeebd50e26b0a5db2a29
tyranron <tyranron@gmail.com>
2018-03-14 00:26:48 +0200

    Impl hello world Chrome extension

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,28 @@
 #[macro_use]
 extern crate stdweb;
 
+use stdweb::traits::*;
+use stdweb::web::{
+    document,
+};
+use stdweb::web::event::ClickEvent;
+
+macro_rules! enclose {
+    ( ($( $x:ident ),*) $y:expr ) => {
+        {
+            $(let $x = $x.clone();)*
+            $y
+        }
+    };
+}
+
 fn main() {
     stdweb::initialize();
 
-    let message = "Hello, 世界!";
-    js! {
-        alert(@{message});
-    }
+    let h1 = document().query_selector("h1").unwrap().unwrap();
+    h1.add_event_listener( enclose!( (h1) move |_ :ClickEvent| {
+        h1.set_text_content("Hello, 世界!");
+    }) );
 
     stdweb::event_loop();
 }
